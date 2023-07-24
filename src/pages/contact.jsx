52589bd7b5f475632e696c5d6f4e584872adacb6
eftuT <eftutesfaye357@gmail.com>
2023-07-24 16:23:51 +0300

    adding file

---
+++ src/pages/contact.jsx
@@ -0,0 +1,6 @@
+import { AlignCenterHorizontal } from "phosphor-react";
+import React from "react";
+
+export const Contact = () => {
+  return <div align="center"><h1>contact us </h1></div>;
+};
---
+++ src/products.js
@@ -0,0 +1,93 @@
+
+import product1 from "./assets/hp.jpg";
+import product2 from "./assets/dress.jpg";
+import product3 from "./assets/girls.webp";
+import product4 from "./assets/headset.jpg";
+import product5 from "./assets/shirt.webp";
+import product6 from "./assets/shoes.jpg";
+import product7 from "./assets/suit.jpg";
+import product8 from "./assets/hill.jpg";
+import product9 from "./assets/bottle.jpg";
+import product10 from "./assets/eyeglass.jpg";
+import product11 from "./assets/necklace.jpg";
+import product12 from "./assets/slipper.jpg";
+import product13 from "./assets/watch.webp";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "HP",
+    price: 900,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "DRESS",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "GIRLS",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "HEADSET",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "SHIRT",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SHOES",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "SUIT",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "HILL",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "BOTTLE",
+    price: 228.0,
+    productImage: product9},
+    {
+      id: 10,
+      productName: "EYEGLASS",
+      price: 228.0,
+      productImage: product10},
+      {
+        id: 11,
+        productName: "NECKLACE",
+        price: 228.0,
+        productImage: product11},
+        {
+          id: 12,
+          productName: "SLIPPER",
+          price: 228.0,
+          productImage: product12},
+          {
+            id: 13,
+            productName: "WATCH",
+            price: 228.0,
+            productImage: product13,
+          },
+         
+];
